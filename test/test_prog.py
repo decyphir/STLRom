5b600dc411c82e7890bb1f59770a906b50c8b613
JejeLeBoss <jeremy610@hotmail.com>
2022-05-30 15:27:36 +0200

    test_prog in test folder and adding path to .so in test_prog.py

--- test/test_prog.py
+++ test/test_prog.py
@@ -1,4 +1,9 @@
+import sys
+sys.path.append('../build')
 import pyrobonTL
 
-while True :
-	p=pyrobonTL.read_point()
+driver=pyrobonTL.STLDriver()
+phi=pyrobonTL.STLMonitor()
+driver.parse_file("spec.stl")
+phi=driver.get_monitor("phi")
+phi.add_sample([1,2,3])
