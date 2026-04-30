f6185dc00d4c3747d54a5cd02d51685855d1af2f
Hadi Dayekh <hadidayekh@gmail.com>
2026-04-30 14:08:32 +0200

    changed plot function to avoid plot_sample multiple times, runs 10-15 times faster now

--- src/stlrom/signal.py
+++ src/stlrom/signal.py
@@ -1,6 +1,8 @@
-def plot(self, label=None, ax=None, title='Signal Plot'):
+def plot(self, label=None, ax=None, title='Signal Plot', **kwargs):
     import matplotlib.pyplot as plt
 
+    draw_canvas = kwargs.pop('draw_canvas', True)
+    
     samples_list = self.get_samples_list()
 
     if ax is None:
@@ -14,45 +16,55 @@ def plot(self, label=None, ax=None, title='Signal Plot'):
     ax.set_title(title)
     ax.grid(True)
         
-    def plot_sample(s, dt, color=None):
-        if color is None:
-            l, = ax.plot(s.time, s.value, marker='o')
-            color = l.get_color()
-            l = ax.plot([s.time, s.time+dt], [s.value, s.value_at(s.time+dt)], color=color, linestyle='-')
-        else:
-            l = ax.plot(s.time, s.value, marker='o', color=color)
-            l = ax.plot([s.time, s.time+dt], [s.value, s.value_at(s.time+dt)], color=color, linestyle='-')
-        return color,l
-    
-    if len(samples_list) > 1:
-        s = samples_list[0]
-        sn = samples_list[1]
-        dt = sn.time-s.time
-            
-        col,l = plot_sample(s, dt)
-                
-        for i in range(1, len(samples_list)-1):
-            s = samples_list[i]
-            sn = samples_list[i+1]
-            dt = sn.time-s.time
-            plot_sample(s,dt,color=col)
-    elif len(samples_list) == 1:
-        sn = samples_list[0]
-        col = None
-    else:
+    import numpy as np
+    if len(samples_list) == 0:
         print(f"{label}: No samples to plot.")
         return ax
-    
-    col, l = plot_sample(sn, self.end_time-sn.time, col)
-    l[0].set_label(label)
-    #ax.plot(sn.time, sn.value, marker='o', color=col)
+
+    times = []
+    values = []
+
+    for i in range(len(samples_list) - 1):
+        s = samples_list[i]
+        sn = samples_list[i + 1]
+
+        times += [s.time, sn.time, np.nan]
+        values += [s.value, s.value_at(sn.time), np.nan]
+
+    # Last segment (to end_time)
+    sn = samples_list[-1]
+    times += [sn.time, self.end_time, np.nan]
+    values += [sn.value, sn.value_at(self.end_time), np.nan]
+
+    l_line, = ax.plot(times, values, linestyle='-')
+
+    c = l_line.get_color()
+
+    ax.plot(
+        [s.time for s in samples_list],
+        [s.value for s in samples_list],
+        linestyle='None',
+        marker='o',
+        color=c
+    )
+
+    l_line.set_label(label)
+
     ax.legend()
-    ax.figure.canvas.draw()
+
+    if draw_canvas:
+        ax.figure.canvas.draw()
 
     return ax
 
 def plot_rob_map(rob_map, ax=None, same_figure=False, title='Robustness Map'):
     keys = sorted(rob_map, key=len)
-    ax = rob_map[keys[0]].plot(label=keys[0], ax=ax, title=title)
+    ax = rob_map[keys[0]].plot(label=keys[0], ax=ax, title=title, draw_canvas=False)
     for key in keys[1:]:
-        ax = rob_map[key].plot(label=key, ax=ax if same_figure else None, title=title)
+        if not same_figure:
+            ax.figure.canvas.draw()
+        ax = rob_map[key].plot(label=key, ax=ax if same_figure else None, title=title, draw_canvas=False)
+    ax.figure.canvas.draw()
+
+    # return last ax
+    return ax
