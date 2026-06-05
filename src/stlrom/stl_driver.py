866c523ecd6175aea3b6d14a11771976bc413a06
Alexandre Donze <alexandre.donze@univ-grenoble-alpes.fr>
2026-06-05 19:17:34 +0200

    fix for get_signal/plot_signal

--- src/stlrom/stl_driver.py
+++ src/stlrom/stl_driver.py
@@ -2,24 +2,37 @@ from ._stlrom import Signal
 from .signal import plot
 
 def get_signal_idx(self, sig):    
-    sig_list = self.get_signals_names().split()    
-    return sig_list.index(sig) if sig in sig_list else -1
+    if sig in self.signal_map:
+        idx= self.signal_map[sig]
+    else:
+        idx=-1
+    return idx
+
+def get_signame_from_idx(self,idx):
+    signame=''
+    for s in self.signal_map:
+        if self.signal_map[s]==idx:
+            signame= s
+            break
+    return signame
 
 def get_signal(self, sig):
+
     if isinstance(sig, int):
         idx = sig
     else:
         idx = get_signal_idx(self, sig)
     
-    sig = Signal()
+    sig = None
     if idx!=-1:
-        for sample in self.data:
-            sig.append_sample(sample[0], sample[idx+1])
+        sig= self.data[idx]
+
     return sig
 
 def plot_signal(self, sig, label=None, ax=None):
     S = self.get_signal(sig)
-    if label is None: 
+    
+    if S is not None and label is None: 
         label = str(sig) #FIXME: dummy if sig is integer
     return plot(S, label=label, ax=ax)
     
