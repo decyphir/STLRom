8c099f1e9865e71a6d85e9641854f2e375e8691c
Hadi Dayekh <hadi.dayekh@univ-grenoble-alpes.fr>
2026-07-21 14:02:10 +0200

    added certain methods to stl_data

--- include/stl_data.h
+++ include/stl_data.h
@@ -9,12 +9,91 @@ namespace STLRom
     class STLData
     {
         public:
-            trace_data data;
+            trace_data data_vector; // data_vector is a vector of signals
+            map<string, int> signal_map; 
 
             STLData() = default;
 
             ~STLData() = default;
+
+            // constructor with number of signals
+            explicit STLData(int nb_signals) : data_vector(nb_signals) {}
+
+            // constructor with vector of signals
+            explicit STLData(trace_data data_vector) : data_vector(std::move(data_vector)) {}
+
+            // other necessary constructors
+            // Copy constructor
+            STLData(const STLData &other)
+                : data_vector(other.data_vector)
+            {
+            }
+
+            // Copy assignment operator
+            STLData &operator=(const STLData &other)
+            {
+                if (this != &other)
+                {
+                    data_vector = other.data_vector;
+                }
+                return *this;
+            }
+
+            // Move constructor
+            STLData(STLData &&other) noexcept
+                : data_vector(std::move(other.data_vector))
+            {
+            }
+
+            // Move assignment operator
+            STLData &operator=(STLData &&other) noexcept
+            {
+                if (this != &other)
+                {
+                    data_vector = std::move(other.data_vector);
+                }
+                return *this;
+            }
+
+            // add sample
+            void add_sample(vector <double> sample);
+
+            void add_sample(vector <double> sample, bool interp);
+            // add constant sample
+
+
+            // get size (nb of signals)
+            int get_size() const;
+
+            // reset signal data
+            void reset_signal_data();
+
+            // check if ALL signals are empty
+            bool is_empty() const;
+
+            friend ostream &operator<<(ostream &out, const STLData &data)
+            {
+                if (data.is_empty())
+                {
+                    out << "No data yet.";                  
+                }
+                else
+                {
+                    for (const auto &signal : data.signal_map)
+                    {
+                        out << "# Signal " << signal.first << ":"<< endl;
+                        if (data.data_vector[signal.second].empty())
+                        {
+                            out << "No data yet." << endl;
+                        }
+                        else
+                        {
+                            out << data.data_vector[signal.second].size() << " samples from t0=" << data.data_vector[signal.second].beginTime << " to t_end=" << data.data_vector[signal.second].endTime << endl;
+                        }
+                    }
+                }  
+            }
     };
 }
 
-#endif STL_DATA_H
+#endif
---
+++ src/stl_data.cpp
@@ -0,0 +1,47 @@
+#include "stl_data.h"
+
+namespace STLRom
+{
+    int STLData::get_size() const
+    {
+        return data_vector.size();
+    }
+
+    void STLData::reset_signal_data()
+    {
+        for (auto &signal : data_vector)
+            {
+                signal.clear();
+            }
+    }
+
+    bool STLData::is_empty() const
+    {
+        return std::all_of(data_vector.begin(), data_vector.end(),
+            [](const Signal& s) { return s.empty(); });
+    }
+
+
+    void STLData::add_sample(vector <double> sample, bool interp)
+    {
+        if (sample.size() != get_size() + 1) 
+	    {
+        	throw std::invalid_argument("Sample size does not match the number of signals.");
+    	}
+
+        double t = sample[0];
+        for (int i = 1; i < sample.size(); i++) 
+        {
+            data_vector[i - 1].appendSample(t, sample[i], 0., interp);
+        }
+
+        // TODO: uptodate?
+    }
+
+    void STLData::add_sample(vector <double> sample)
+    {
+        add_sample(sample, true);
+    }
+
+
+}
