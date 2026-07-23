e403c2534c37cf53fc7b9aeea72c9fec235bf6e7
Hadi Dayekh <hadidayekh@gmail.com>
2026-07-23 13:32:49 +0200

    added test_stl_data

---
+++ test/test_stl_data.cpp
@@ -0,0 +1,40 @@
+    #include <stdio.h>
+#include "stl_data.h"
+#include <string>
+#include <vector>
+
+using namespace std;
+using namespace STLRom;
+
+int main(int argc, char** argv) {
+
+    
+    STLData data_empty = STLData();
+
+    cout << "Empty constructor:" << endl;
+    cout << data_empty << endl << endl;
+
+    STLData data = STLData(3);
+
+    cout << "Initializing with 3 empty signals:" << endl;
+    cout << data << endl << endl;
+
+    data.add_sample({0., 0., 2., 3.});
+    data.add_sample({1., 10., -2., 4.});
+    data.add_sample({2.5, 0., 3., 8.});
+    data.add_sample({3., 1., 2., 0.});
+
+    cout << data << endl << endl;
+
+    cout << "Resetting then printing." << endl;
+
+    data.reset_signal_data();
+    cout << data << endl << endl;
+
+    cout << "Testing display_signal_names():" << endl;
+    data.display_signal_names();
+    cout << endl;
+
+    return 0;
+
+}
