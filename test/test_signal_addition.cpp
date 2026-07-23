94bf9b4662d99f922332e8952aa715f96026fa2c
Joris Tillet <joris.tillet@univ-grenoble-alpes.fr>
2026-07-23 11:49:27 +0200

    addition of signals using merge_signals tool func

--- test/test_signal_addition.cpp
+++ test/test_signal_addition.cpp
@@ -5,30 +5,78 @@ using namespace STLRom;
 
 int main(int argc, char **argv)
 {
-    // TOFIX problem handling interpolation
-    return 0;
-    // Signal sig1 = Signal(0., 0., 1);
-    // sig1.appendSample(2., 2.);
-    // sig1.appendSample(3., 1.);
-    // sig1.appendSample(6., 4.);
-    // sig1.appendSample(12., -2.);
+    Signal sig1, sig2;
+    Signal result, expected_sig;
 
-    // Signal sig2 = Signal(4., 1., 1);
-    // sig2.appendSample(6., -1.);
-    // sig2.appendSample(8., 0.);
-    // sig2.appendSample(15., 3.);
+    // Test 1 - Simple
+    sig1 = Signal(0., 0., 1);
+    sig1.appendLinearSample(2., 2.);
+    sig1.appendLinearSample(4., 0.);
 
-    // Signal result = sig1 + sig2;
+    sig2 = Signal(0., 1., 1);
+    sig2.appendConstantSample(4., 1.);
+    
+    expected_sig = Signal();
+    expected_sig.appendLinearSample(0., 1.);
+    expected_sig.appendLinearSample(2., 3.);
+    expected_sig.appendLinearSample(4., 1.);
+    
+    result = sig1 + sig2;
+    // cout << expected_sig << endl;
     // cout << result << endl;
+    if (expected_sig != result) {
+        return 1;
+    }
 
-    // Signal expected_sig = Signal(0., 0., 1);
-    // expected_sig.appendSample(2., 2.);
-    // expected_sig.appendSample(3., 1.);
-    // expected_sig.appendSample(4., 3.);
-    // expected_sig.appendSample(6., 3.);
-    // expected_sig.appendSample(8., 2.);
-    // expected_sig.appendSample(12., 12./7-2.);
-    // expected_sig.appendSample(15., 3.);
+    // Test 2 - Piecewise linear
+    sig1 = Signal(0., 0., 1);
+    sig1.appendLinearSample(2., 2.);
+    sig1.appendLinearSample(3., 1.);
+    sig1.appendLinearSample(6., 4.);
+    sig1.appendLinearSample(12., -2.);
 
-    // return int(expected_sig != result);
+    sig2 = Signal();
+    sig2.appendSample(0., 1., -.5);
+    sig2.appendConstantSample(4., 1.);
+    sig2.appendConstantSample(9., 1.);
+    sig2.appendLinearSample(12., -2.);
+    
+    expected_sig = Signal();
+    expected_sig.appendLinearSample(0., 1.);
+    expected_sig.appendLinearSample(2., 2.);
+    expected_sig.appendLinearSample(3., .5, .5);
+    expected_sig.appendConstantSample(4., 3.);
+    expected_sig.appendLinearSample(6., 5.);
+    expected_sig.appendLinearSample(9., 2.);
+    expected_sig.appendLinearSample(12., -4.);
+
+    result = sig1 + sig2;
+    // cout << expected_sig << endl;
+    // cout << result << endl;
+    if (expected_sig != result) {
+        return 1;
+    }
+
+
+    // Test 3 - Simplify
+    sig1 = Signal(0., 0., 1);
+    sig1.appendLinearSample(2., 2.);
+    sig1.appendLinearSample(4., 6.);
+
+    sig2 = Signal(0., 1., 1);
+    sig2.appendLinearSample(2., 1.);
+    sig2.appendLinearSample(4., -1.);
+    
+    expected_sig = Signal();
+    expected_sig.appendLinearSample(0., 1.);
+    expected_sig.appendLinearSample(4., 5.);
+
+    result = sig1 + sig2;
+    // cout << expected_sig << endl;
+    // cout << result << endl;
+    if (expected_sig != result) {
+        return 1;
+    }
+
+    return 0;
 }
